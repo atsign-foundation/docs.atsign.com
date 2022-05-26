c327a3929b0d74ef67f6868f26ccd5e8db9959c8
XavierChanth <xchanthavong@gmail.com>
2022-05-26 15:47:10 -0400

    Update purgeCSS technique to prevent purging necessary css

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,38 +1,31 @@
 const autoprefixer = require('autoprefixer');
 const purgecss = require('@fullhuman/postcss-purgecss');
-const whitelister = require('purgecss-whitelister');
+
+const attributes = [
+  'data-dark-mode',
+  'completed',
+  'selected',
+  'disappear',
+  'open',
+  'data-global-alert',
+  'id'
+];
 
 module.exports = {
   plugins: [
     autoprefixer(),
     purgecss({
-      content: [
-        './node_modules/@hyas/doks/layouts/**/*.html',
-        './node_modules/@hyas/doks/content/**/*.md',
-        './layouts/**/*.html',
-        './content/**/*.md',
-      ],
-      safelist: [
-        'lazyloaded',
-        'table',
-        'thead',
-        'tbody',
-        'tr',
-        'th',
-        'td',
-        ...whitelister([
-          './node_modules/@hyas/doks/assets/scss/common/_variables.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_doks.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_code.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_buttons.scss',
-          './node_modules/@hyas/doks/assets/scss/components/_search.scss',
-          './node_modules/@hyas/doks/assets/scss/common/_dark.scss',
-          './assets/scss/common/_variables.scss',
-          './assets/scss/components/_overrides.scss',
-          './assets/scss/common/_dark.scss',
-          './node_modules/katex/dist/katex.css',
-        ]),
-      ],
+      content: ['./hugo_stats.json'],
+      defaultExtractor: content => {
+        const els = JSON.parse(content).htmlElements;
+        return [
+          ...(els.tags || []),
+          ...(els.classes || []),
+          ...(els.ids || []),
+          ...attributes,
+        ];
+      },
+      safelist: []
     }),
   ],
 };
